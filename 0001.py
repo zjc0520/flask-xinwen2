9c8bc95a1812a221da9a00f6be0b4762e6d2263a
zjc0520 <1010266052@qq.com>
2018-09-26 22:08:46 +0800

    第二班

--- 0001.py
+++ 0001.py
@@ -5,5 +5,9 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     return 'helloworld'
+@app.route('/')
+def index():
+    return 'helloworld'
+
 if __name__ == '__main__':
     app.run(debug=True)
