e3eadef74c3fac5ddefa5cce266ad72995586dc1
zjc0520 <1010266052@qq.com>
2018-09-27 15:59:02 +0800

    基础配置1

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import redis
+import logging
 
 class Config(object):
     """工程配置信息"""
@@ -18,10 +19,9 @@ class Config(object):
     SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
     SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
     SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
-    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
+    PERMANENT_SESSION_LIFETIME = 86400# session 的有效期# ，单位是秒
 
-
-
+    LOG_level=logging.DEBUG
 class DevelopementConfig(Config):
     """开发模式下的配置"""
     DEBUG = True
@@ -29,7 +29,7 @@ class DevelopementConfig(Config):
 
 class ProductionConfig(Config):
     """生产模式下的配置"""
-    pass
+    LOG_LEVEL=logging.ERROR
 
 # 定义配置字典
 config = {
