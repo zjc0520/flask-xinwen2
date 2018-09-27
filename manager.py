bac2312004e8d899af2fb6bc7db621db9d0094df
zjc0520 <1010266052@qq.com>
2018-09-27 15:43:03 +0800

    项目基础配置

---
+++ manager.py
@@ -0,0 +1,16 @@
+from flask_script import Manager
+from flask_migrate import Migrate,MigrateCommand
+from info import create_app,db
+app=create_app("development")
+## Flask-script
+manager=Manager(app)
+
+#数据库迁移
+Migrate(app,db)
+manager.add_command("db",MigrateCommand)
+
+@app.route('/index')
+def index():
+    return "index"
+if __name__ == '__main__':
+    manager.run()
