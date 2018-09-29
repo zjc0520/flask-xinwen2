83d732075a347c274330b0c946f685b6dccff4e1
zjc_jh <1010266052@qq.com>
2018-09-29 08:58:54 +0800

    注册跟路由

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,5 +1,11 @@
 from . import index_blu
+from flask import render_template, current_app
+
+
 # 使用蓝图
 @index_blu.route('/index')
 def index():
-    return 'index'
+    return render_template("news/index.html")
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
